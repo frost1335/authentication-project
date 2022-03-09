8d807a7bb4fd7978957c26dc3387017cca15a562
raximvodil <raximvodilrozbek@gmail.com>
2022-03-10 04:43:40 +0500

    debugged

--- client/src/components/screens/LoginScreen.js
+++ client/src/components/screens/LoginScreen.js
@@ -37,7 +37,7 @@ const LoginScreen = ({ history }) => {
 
       history.push("/");
     } catch (e) {
-      setError(error.response.data.error);
+      setError(e.response.data.error);
       setTimeout(() => {
         setError("");
       }, 5000);
